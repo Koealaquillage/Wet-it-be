4f8caf7f2692218c9d5a59c2fcdb1ac9545a1078
Koealaquillage <gkoenig.lamon@gmail.com>
2024-12-23 19:39:17 +0100

    Create a more complex version of the app

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from app.routers import weather
 
 app = FastAPI()
 
+# Include routers
+app.include_router(weather.router)
+
+@app.get("/")
+async def read_root():
+    return {"message": "Welcome to And Wet It Be!"}
+
 @app.get("/health")
-def read_root():
+async def health_check():
     return {"status": "healthy"}
